c6fb72f6d29bbefb9455c5bb39ba83e33d583dd7
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2024-06-06 22:02:16 +0600

    fixed links and changed styles

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,67 +1,59 @@
-import { useState } from "react"
-import { useLogin } from "../hooks/useLogin"
+import { useState } from "react";
+import { useLogin } from "../hooks/useLogin";
 import { Link, useLocation } from "react-router-dom";
 
 const Login = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const {login, error, isLoading} = useLogin()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login, error, isLoading } = useLogin();
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    await login(email, password)
-  }
+    await login(email, password);
+  };
 
   return (
     <div className="container2">
       <div className="cover2">
+        <form onSubmit={handleSubmit}>
+          <h1>Login</h1>
+          {error && <span className="error-message">{error}</span>}
+          <input
+            type="email"
+            value={email}
+            placeholder="Enter email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
 
-      <form onSubmit={handleSubmit}>
+          <button disabled={isLoading}>Log in</button>
 
-<h1>Login</h1>
-{error && <span className="error-message">{error}</span>}
-<input
-  type="email"
-  value={email}
-  placeholder="Enter email"
-  onChange={(e) => setEmail(e.target.value)} 
+          {/* <Link className="link1" to="/Forget" tabIndex={4}>
+            Forgot Password ?
+          </Link> */}
 
-/>
-<input
-  type="password"
-  placeholder="Password"
-  onChange={(e) => setPassword(e.target.value)} 
-  value={password} 
-/>
-
-<button disabled={isLoading}>Log in</button>
-
-
-
-<Link 
-  className="link1" 
-  to="/Forget"
-  tabIndex={4}>
-  Forgot Password ?
-</Link>
-
-
-
-
-<span className="link1">
-  Don't have an account? <Link to="/Register" tabIndex={5}>Register</Link>
-</span>
-
-
-
+          <span className="link1">
+            Don't have an account?{" "}
+            <Link to="/signup" tabIndex={5}>
+              Register
+            </Link>
+          </span>
         </form>
         <div className="pic">
-                    <img src="https://img.freepik.com/free-vector/computer-login-concept-illustration_114360-7962.jpg?size=338&ext=jpg&uid=R95233801&ga=GA1.1.1100157101.1672301754&semt=ais" alt="fieldWithGrains"></img>
-                </div>
+          <img
+            src="https://img.freepik.com/free-vector/computer-login-concept-illustration_114360-7962.jpg?size=338&ext=jpg&uid=R95233801&ga=GA1.1.1100157101.1672301754&semt=ais"
+            alt="fieldWithGrains"
+          ></img>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,93 +1,92 @@
-import { useState } from "react"
-import { useSignup } from "../hooks/useSignup"
+import { useState } from "react";
+import { useSignup } from "../hooks/useSignup";
 import { Link, useLocation } from "react-router-dom";
 
-
 const Signup = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [checkpassword, setCheckPassword] = useState('')
-  const[username, setUsername] = useState('')
-  const[Phone, setPhone] = useState('')
-  const[NID, setNID] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [checkpassword, setCheckPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [Phone, setPhone] = useState("");
+  const [NID, setNID] = useState("");
 
-  const {signup, error, isLoading} = useSignup()
+  const { signup, error, isLoading } = useSignup();
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    await signup(username, Phone, NID,email, password, checkpassword)
-  }
+    await signup(username, Phone, NID, email, password, checkpassword);
+  };
 
   return (
     <div className="container1">
-    {/* <Navigate to="/Accountverification" /> */}
-        <div className="cover1">
+      {/* <Navigate to="/Accountverification" /> */}
+      <div className="cover1">
+        <form onSubmit={handleSubmit}>
+          <h1>Create account</h1>
 
-            <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Enter username"
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+          />
 
-            <h1>Create account</h1>
- 
-  <input 
-    type="text" 
-    placeholder="Enter username"
-    onChange={(e) => setUsername(e.target.value)} 
-    value={username} 
-  />
+          <input
+            type="number"
+            placeholder="Enter NID"
+            onChange={(e) => setNID(e.target.value)}
+            value={NID}
+          />
+          <input
+            type="number"
+            placeholder="Enter Phone"
+            onChange={(e) => setPhone(e.target.value)}
+            value={Phone}
+          />
 
- <input 
-    type="number" 
-    placeholder="Enter NID"
-    onChange={(e) => setNID(e.target.value)} 
-    value={NID} 
-  />
-  <input 
-    type="number" 
-    placeholder="Enter Phone"
-    onChange={(e) => setPhone(e.target.value)} 
-    value={Phone} 
-  />
+          <input
+            type="email"
+            placeholder="Enter email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
 
-  <input 
-    type="email" 
-    placeholder="Enter email"
-    onChange={(e) => setEmail(e.target.value)} 
-    value={email} 
-  />
- 
-  <input 
-    type="password" 
-    placeholder="Enter password"
-    onChange={(e) => setPassword(e.target.value)} 
-    value={password} 
-  />
+          <input
+            type="password"
+            placeholder="Enter password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
 
- <input 
-    type="password" 
-    placeholder="Check Password"
-    onChange={(e) => setCheckPassword(e.target.value)} 
-    value={checkpassword} 
-  />
+          <input
+            type="password"
+            placeholder="Check Password"
+            onChange={(e) => setCheckPassword(e.target.value)}
+            value={checkpassword}
+          />
 
+          <button disabled={isLoading}>Sign up</button>
 
-<button disabled={isLoading}>Sign up</button>
+          <span>
+            Already have an account?&nbsp;
+            <Link to="/login" disabled={isLoading}>
+              Login here
+            </Link>
+          </span>
 
-  <span>
-            Already have an account? 
-                    <Link to="/login" disabled = {isLoading} >Login</Link>
-                </span>
-
-  {error && <div className="error">{error}</div>}
-</form>
-          <div className="pic">
-                <img src="https://img.freepik.com/free-vector/sign-up-concept-illustration_114360-7965.jpg?size=338&ext=jpg&uid=R95233801&ga=GA1.1.1100157101.1672301754&semt=ais
-" alt="fieldWithGrains"></img>
-            </div>
+          {error && <div className="error">{error}</div>}
+        </form>
+        <div className="pic">
+          <img
+            src="https://img.freepik.com/free-vector/sign-up-concept-illustration_114360-7965.jpg?size=338&ext=jpg&uid=R95233801&ga=GA1.1.1100157101.1672301754&semt=ais
+"
+            alt="fieldWithGrains"
+          ></img>
         </div>
-
+      </div>
     </div>
-)
+  );
+};
 
-}
-
-export default Signup
+export default Signup;
