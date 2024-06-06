d0ce7b1c74555d79cae0fa848ffb6bc314db7e49
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2024-06-06 23:14:09 +0600

    fixed styling

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -11,6 +11,8 @@ import { addProduct } from "../redux/cartRedux";
 import { useDispatch } from "react-redux";
 import Exchangerequest from "../components/Exchangerequest"
 import axios from "axios";
+import { useParams } from "react-router-dom";
+
 
 const Container = styled.div``;
 
@@ -35,16 +37,17 @@ const InfoContainer = styled.div`
 `;
 
 const Title = styled.h1`
-  font-weight: 200;
+  font-weight: 500;
+  color:teal;
 `;
 
 const Desc = styled.p`
-  margin: 20px 0px;
+  margin: 10px 0px;
 `;
 
 const Price = styled.span`
   font-weight: 100;
-  font-size: 40px;
+  font-size: 30px;
 `;
 
 const FilterContainer = styled.div`
@@ -53,6 +56,12 @@ const FilterContainer = styled.div`
   display: flex;
   justify-content: space-between;
 `;
+
+const OwnerInfo = styled.div`
+  font-size:16px;
+  &.span{
+    font-weight:600;
+  }`
 
 const Filter = styled.div`
   display: flex;
@@ -81,8 +90,9 @@ const FilterSize = styled.select`
 const FilterSizeOption = styled.option``;
 
 const AddContainer = styled.div`
+margin-top:10px;
   width: 50%;
-  display: flex;
+  display: block;
   align-items: center;
   justify-content: space-between;
 `;
@@ -106,44 +116,85 @@ const Amount = styled.span`
   cursor : text;
 `;
 
-const Button = styled.button`
-  width: 15%;
-  padding: 15px;
-  border: 3px solid teal;
+const PrimaryButton = styled.button`
+padding: 8px;
+border: 2px solid teal;
+    background-color: teal;
+    color:white;
+cursor: pointer;
+font-weight: 600;
+font-size: 15px;
+border-radius:10px;
+transition: all 200ms ease;
+
+&:hover{
+    // background-color: #f8f4f9;
+color:teal;
+background-color: white;
+    
+}
+`;
+
+const SecondaryButton = styled.button`
+  // padding: 4px;
+  border: none;
+  color:teal;
   background-color: white;
   cursor: pointer;
-  font-weight: 500;
+  font-weight: 600;
+  font-size: 15px;
+  border-radius:10px;
+  transition: all 200ms ease;
+  // border:1px solid teal;
+  margin-left:5px;
 
   &:hover{
-      background-color: #f8f4f4;
+      // background-color: #f8f4f9;
+      background-color: teal;
+      color:white;
+      
   }
-`;
+`
+
+const CategoryContainer = styled.div`
+display: flex;
+margin-bottom:10px;
+margin-top:4px;
+`
+
+const Category = styled.span`
+background-color:#ccc;
+color: teal;
+padding:2px 6px;
+border-radius: 5px;
+margin-right:5px;
+`
 
 
 
 const Product = () => {
 
-  const location = useLocation();
-  const id = location.pathname.split("/")[2];
+  const { id } = useParams();
   const [product, setProduct] = useState({});
   const [quantity, setQuantity] = useState(1);
   const [color, setColor] = useState("");
   const [size, setSize] = useState("");
-  const [isexchange, setisexchange]= useState(false)
-  const [isrent, setisrent]= useState(false)
-  const [owner, setowner]=useState('')
+  const [isexchange, setisexchange] = useState(false)
+  const [isrent, setisrent] = useState(false)
+  const [owner, setowner] = useState('')
   const dispatch = useDispatch();
 
   const upperuser = JSON.parse(localStorage.getItem('user'))
-  const user=upperuser.user
+  const user = upperuser.user
 
 
   useEffect(() => {
     const getProduct = async () => {
       try {
         const res = await publicRequest.get("/products/find/" + id);
+        console.log(res.data)
         setProduct(res.data);
-      } catch {}
+      } catch { }
     };
     getProduct();
   }, [id]);
@@ -151,50 +202,49 @@ const Product = () => {
 
   useEffect(() => {
     const getuser = async () => {
-      if(product){
-      try{
-        const res = await axios.get('http://localhost:3000/api/user/find/'+product.user_email)
-        setowner(res.data)
-        console.log(res.data)
-      }catch(error)
-      {
-        console.log(error)
+      if (product) {
+        try {
+          const res = await axios.get('http://localhost:3000/api/user/find/' + product.user_email)
+          setowner(res.data)
+          console.log(res.data)
+        } catch (error) {
+          console.log(error)
+        }
       }
-    }
-  };
-  getuser();
-  },[product.user_email]);
+    };
+    getuser();
+  }, [product.user_email]);
 
 
 
-  const handleexchange = (e)=>{
-      setisexchange(current => !current)
+  const handleexchange = (e) => {
+    setisexchange(current => !current)
   }
 
-  const handlerent = async(e) => {
+  const handlerent = async (e) => {
     e.preventDefault()
-    const owner_id=owner._id
-    const sender_id=user._id 
-    console.log("sender",owner_id)
-    const objectid=product._id
-    console.log("product",objectid)
-    const proposed_price=product.price
-    const renttype=product.prefer
-    await axios.post('http://localhost:3000/api/Addition/rentrequest', 
-        {sender_id, owner_id, objectid,proposed_price,renttype}
-        ).then((response)=>{
-            console.log(response)
-            setisrent(true)
-          }).catch((error)=>{
-            if (error.response) {
-              console.log(error.response);
-              console.log("server responded");
-            } else if (error.request) {
-              console.log("network error");
-            } else {
-              console.log(error);
-            }
-          })
+    const owner_id = owner._id
+    const sender_id = user._id
+    console.log("sender", owner_id)
+    const objectid = product._id
+    console.log("product", objectid)
+    const proposed_price = product.price
+    const renttype = product.prefer
+    await axios.post('http://localhost:3000/api/Addition/rentrequest',
+      { sender_id, owner_id, objectid, proposed_price, renttype }
+    ).then((response) => {
+      console.log(response)
+      setisrent(true)
+    }).catch((error) => {
+      if (error.response) {
+        console.log(error.response);
+        console.log("server responded");
+      } else if (error.request) {
+        console.log("network error");
+      } else {
+        console.log(error);
+      }
+    })
   }
 
 
@@ -224,78 +274,62 @@ const Product = () => {
       <Wrapper>
 
         <ImgContainer>
-          
+
           <Image src={product.img} />
-          
+
         </ImgContainer>
 
         <InfoContainer>
-          <Title> <strong> Product Name: </strong> {product.title}</Title>
-          <Desc> <strong> Description: </strong> {product.desc} </Desc>
-          <Desc>
-          {product.categories && product.categories.map((category, index) => (
-          <span key={index}>
-          {category}
-          {index !== product.categories.length - 1 && ', '}
-          </span>
-          ))}
-          </Desc>
-          {product.price && <Price>{product.price}/=</Price>}
+          <Title>{product.title}</Title>
+          <OwnerInfo>Posted By <span>{owner.username}</span><Link to={`/messege?data=${product.user_email}`}>
+            <SecondaryButton>
+              Message
+            </SecondaryButton>
+          </Link></OwnerInfo>
+
+          <Desc>Description<br /> <span>{product.desc}</span> </Desc>
+          {product.categories && product.categories.length > 0 && (
+              <div>Tags</div>
+            )}
+          <CategoryContainer>
+           
+            {product.categories && product.categories.map((category, index) => (
+              <span key={index}>
+                <Category>{category}</Category>
+              </span>
+            ))}
+          </CategoryContainer>
+
+          {product.price && <Price>Tk {product.price}</Price>}
 
           {product.exchangetype && <Desc>
-            {product.exchangetype}
+            Preference: {product.exchangetype}
           </Desc>}
           {product.prefer && <Desc>
             {product.prefer}
           </Desc>}
-          <Title>Owner name: {owner.username}</Title>
 
-          <FilterContainer> </FilterContainer> 
+          {product.purpose === "Exchange" ? <PrimaryButton onClick={handleexchange}>Exchange</PrimaryButton> : null}
+          {product.purpose === "Rent" ? <PrimaryButton onClick={handlerent}>Rent</PrimaryButton> : null}
+          {product.purpose === "Sell" ?
 
-          <Button>
-            <Link to= {`/messege?data=${product.user_email}`}>Message</Link>
-          </Button>
-
-          <FilterContainer>
-            {/* <Filter>
-              <FilterTitle>Color</FilterTitle>
-              <FilterColor color="black" />
-              <FilterColor color="darkblue" />
-              <FilterColor color="gray" />
-            </Filter> }
-            { <Filter>
-              <FilterTitle>Size</FilterTitle>
-              <FilterSize>
-                <FilterSizeOption>XS</FilterSizeOption>
-                <FilterSizeOption>S</FilterSizeOption>
-                <FilterSizeOption>M</FilterSizeOption>
-                <FilterSizeOption>L</FilterSizeOption>
-                <FilterSizeOption>XL</FilterSizeOption>
-              </FilterSize>
-            </Filter> */}
-          </FilterContainer> 
-
-
-
-          {product.purpose==="Exchange"?<Button onClick={handleexchange}>Exchange</Button>:null}
-          {product.purpose==="Rent"?<Button onClick={handlerent}>Rent</Button>:null}
-          {product.purpose==="Sell"?
-
-          <AddContainer>
-            <AmountContainer>
-              <Remove onClick={() => handleQuantity("dec")} />
-              <Amount>{quantity}</Amount>
-              <Add onClick={() => handleQuantity("inc")} />
-            </AmountContainer>
-            <Button onClick={handleClick} >ADD TO CART</Button>
-            
-          </AddContainer>:null}
-        {isexchange&&<Exchangerequest Product={product} />}
-        {isrent&&<label>rent request has been sent</label>}
+            <AddContainer>
+              <AmountContainer>
+                <Remove onClick={() => handleQuantity("dec")} />
+                <Amount>{quantity}</Amount>
+                <Add onClick={() => handleQuantity("inc")} />
+              </AmountContainer>
+              <br/>
+              <div>
+              <PrimaryButton onClick={handleClick} >Add to cart</PrimaryButton>
+</div>
+            </AddContainer> : null}
+          {isexchange && <Exchangerequest Product={product} />}
+          {isrent && <label>rent request has been sent</label>}
 
         </InfoContainer>
 
-      </Wrapper> 
+      </Wrapper>
 
       <Newsletter />
       <Footer />
