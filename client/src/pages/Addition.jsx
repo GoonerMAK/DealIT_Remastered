21988d79a0ea7568c638b032a462520d329efc63
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2024-06-06 22:01:48 +0600

    fixed buggy dropdown

--- client/src/pages/Addition.jsx
+++ client/src/pages/Addition.jsx
@@ -1,13 +1,12 @@
-import { useState } from "react"
+import { useState } from "react";
 import Addexchangeproduct from "../components/Addexchangeproduct";
 import Addsell from "../components/Addsell";
 import Addrent from "../components/Addrent";
-import styled from "styled-components"
+import styled from "styled-components";
 import Announcement from "../components/Announcement";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Newsletter from "../components/Newsletter";
-
 
 const Selection = styled.select`
   width: 20%;
@@ -22,9 +21,7 @@ const Selection = styled.select`
   }
 `;
 
-
-const Container = styled.div`
-`;
+const Container = styled.div``;
 
 const FormContainer = styled.div`
   min-height: 105vh;
@@ -33,7 +30,7 @@ const FormContainer = styled.div`
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
-  padding:10px 0;
+  padding: 10px 0;
 `;
 
 const DropdownContainer = styled.div`
@@ -43,58 +40,36 @@ const DropdownContainer = styled.div`
   align-items: center;
 `;
 
-
 const Addition = () => {
+  const [selected, setSelected] = useState("Sell");
 
-    const [selected, setselected] = useState('')
-    const [exchange, setexchange] = useState('')
-    const [sell, setsell] = useState(true)
-    const [rent, setrent] = useState('')
+  const handleSelect = (e) => {
+    setSelected(e.target.value);
+  };
 
+  return (
+    <>
+      <Container>
+        <Announcement />
+        <Navbar />
 
-    const handleselect = (e) => {
-        setselected(e.target.value)
-        Check()
-    }
-    const Check = () => {
-        if (selected === "Exchange") {
-            setexchange(true)
-            setrent(false)
-            setsell(false)
-        } else if (selected === 'Rent') {
-            setrent(true)
-            setexchange(false)
-            setsell(false)
-        } else if (selected === "Sell") {
-            setsell(true)
-            setrent(false)
-            setexchange(false)
-        }
-    }
-    return (
-        <>
-            <Container>
-                <Announcement />
-                <Navbar />
+        <FormContainer>
+          <DropdownContainer>
+            <Selection value={selected} onChange={handleSelect}>
+              <option value="Sell">Sell</option>
+              <option value="Rent">Rent</option>
+              <option value="Exchange">Exchange</option>
+            </Selection>
+          </DropdownContainer>
+          {selected === "Exchange" && <Addexchangeproduct />}
+          {selected === "Sell" && <Addsell />}
+          {selected === "Rent" && <Addrent />}
+        </FormContainer>
+      </Container>
+      <Newsletter />
+      <Footer />
+    </>
+  );
+};
 
-                <FormContainer>
-
-                    <DropdownContainer>
-                        <Selection value={selected} onChange={handleselect}>
-                            <option value="Sell">Sell</option>
-                            <option value="Rent">Rent</option>
-                            <option value="Exchange">Exchange</option>      {/*value={exchange} onSelect={setexchange(true)}*/}
-                            <option>others</option>
-                        </Selection>
-                    </DropdownContainer>
-                    {exchange && <Addexchangeproduct />}
-                    {sell && <Addsell />}
-                    {rent && <Addrent />}
-                </FormContainer>
-            </Container>
-            <Newsletter />
-            <Footer />
-        </>
-    )
-}
-export default Addition
+export default Addition;
