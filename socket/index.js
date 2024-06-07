3a3dfe6cc8e0d0685b715edee7209a6dfafe1765
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2024-06-07 18:18:31 +0600

    null error solved

--- socket/index.js
+++ socket/index.js
@@ -7,16 +7,16 @@ const io = require("socket.io")(8900, {
 let users = [];
 
 const addUser = (userId, socketId) => {
-  !users.some((user) => user.userId === userId) &&
+  !users.some((user) => user?.userId === userId) &&
     users.push({ userId, socketId });
 };
 
 const removeUser = (socketId) => {
-  users = users.filter((user) => user.socketId !== socketId);
+  users = users.filter((user) => user?.socketId !== socketId);
 };
 
 const getUser = (userId) => {
-  return users.find((user) => user.userId === userId);
+  return users.find((user) => user?.userId === userId);
 };
 
 io.on("connection", (socket) => {
@@ -25,14 +25,14 @@ io.on("connection", (socket) => {
 
   //take userId and socketId from user
   socket.on("addUser", (userId) => {
-    addUser(userId, socket.id);
+    addUser(userId, socket?.id);
     io.emit("getUsers", users);
   });
 
   //send and get message
   socket.on("sendMessage", ({ senderId, receiverId, text }) => {
     const user = getUser(receiverId);
-    io.to(user.socketId).emit("getMessage", {
+    io.to(user?.socketId).emit("getMessage", {
       senderId,
       text,
     });
@@ -41,7 +41,7 @@ io.on("connection", (socket) => {
   //when disconnect
   socket.on("disconnect", () => {
     console.log("a user disconnected!");
-    removeUser(socket.id);
+    removeUser(socket?.id);
     io.emit("getUsers", users);
   });
 });
